2afa8d1d22774ddb5537203dfd872ed36d7190fb
GouthaM-Krish-nA <gouthamkp0@gmail.com>
2024-11-11 11:04:49 +0530

    designing the buttons

---
+++ web_dev_lvl_0/practise/practise3/script.js
@@ -0,0 +1,101 @@
+// var x=10;
+// console.log(x)
+// x="Goutham"
+// console.log(x)
+// x=15.454
+// console.log(x);
+// var y =15;
+// var z=x+y;
+// console.log(z);
+// var name= "goutham and i am a student";
+// console.log(name.length);
+// var x=11;
+// if (x>=10) {
+//     console.log("inside if block");
+//     if (x==11) {
+//         console.log("inside if inside if");
+        
+//     }
+// }else if (x==3){
+//         console.log("INSIDE if ELSE BLOCK");
+// }
+// else{
+//     console.log("else block");
+    
+// }
+
+// for (var x = 0; x<=10; x++) {
+//     console.log(x); 
+    
+    
+// }
+// var x=prompt("enter a number from 1-4");
+// console.log(typeof x);
+// a=parseInt(x)
+// string is immutable and also parseInt doesnt make change in x but to
+// console.log(typeof a);
+// switch (x) {
+//     case 1:
+//         console.log("1st case is executed");
+        
+//         break;
+//     case 2:
+//         console.log("second case is executed");
+//         break;
+//     case 3:
+//         console.log("third case is executed");
+//         break;
+//     default:
+//         console.log("enter a value from 1-3");
+//         break;
+// }
+// var name = "goutham";
+// console.log(name.length);
+// console.log(name[6]);
+// // strings are immutable
+
+// var arr = [100, 200, 548, 846, 5487,1472];
+// arr[6]=43;
+// arr[0]=150;
+//  arr[8]=32;  //see the output that seventh space is empty
+//  arr.pop();
+//  arr.shift(); // removes the first element
+//  arr.push(46);
+//  arr.push(8);
+// arr.unshift(46); // adds one elemnt in front
+// console.log(arr);
+// pop() , push() , shift(), unshift()
+// // array is mutable
+// var car = {
+//     name: "alto",
+//     yearOfmanufacturing: 2019,
+//     airbags: false,
+//     brandName: "Maruti Suzuki",
+
+// };
+// car.noOfSeats = 4; //adding new elements in car
+// delete car.airbags;
+// delete car.noOfSeats;
+// car.brandName="Tata";
+// console.log(car.brandName);
+// // console.log(car);   
+// var x=15;
+// var y=20;
+// function add(a,b){
+//     var z=a+y;
+//     console.log(z);
+// }
+
+// // add(x,y);
+var btn = document.getElementById("btn");
+btn.addEventListener
+
+
+
+
+
+
+
+
+
+
